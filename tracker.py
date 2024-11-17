009e0389475779abe1751ac936a5e0a614e5126c
Scotasloth <rossmcc76@gmail.com>
2024-11-17 19:20:03 +0000

    Began work on adding new games to the database

--- tracker.py
+++ tracker.py
@@ -13,20 +13,20 @@ def main():
     program_directory = sys.path[0]
     print (program_directory)
 
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run browser in background 
+
     root = CTk(className="Trophy Tracker") 
-    root.geometry("790x700")
+    root.geometry("590x500")
     root.title("Trophy Tracker")
 
     set_appearance_mode("dark")
 
-    CTkButton(master = root, text = "Initialize", command = create(program_directory)).place(relx = .1, rely = .5)
+    CTkButton(master = root, text = "Initialize", command = create(program_directory)).place(relx = .01, rely = .5)
+    CTkButton(master = root, text = "Add new Trophy").place(relx = .7, rely = .5)
+    CTkButton(master = root, text = "Add new game", command = newGame(root, chrome_options)).place(relx = .4, rely = .5)
 
     root.mainloop()
-    
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Run browser in background 
-
-    #getWebPage(chrome_options)
 
 def create(dir):
     db_name = 'gamedata.accdb'
@@ -44,6 +44,18 @@ def create(dir):
     # Close the database connection
     database.close()
 
+def newGame(root, chrome_options):
+    game = ""
+    addGame = CTkToplevel(root)
+    addGame.title("Add New Game")
+    addGame.geometry("400x300")
+
+    CTkEntry(master = addGame, placeholder_text= "What is the new game?", textvariable = game).place(relx = .2, rely = .5)
+    CTkButton(master = addGame, text = "ENTER", command = getWebPage(game, chrome_options)).place(relx = .2, rely = .7)
+
+def newTrophy(root):
+    return
+
 def connect(db, dir):
     db_path = os.path.join(dir, db)
     conn = pyodbc.connect(r'Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=' + db_path)
@@ -51,7 +63,7 @@ def connect(db, dir):
     print("Connection opened")
     return database
 
-def getWebPage(chrome_options):
+def getWebPage(input, chrome_options):
     url = ""
 
     driver = webdriver.Chrome(options=chrome_options)
