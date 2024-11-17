209a321bb56ca9e810479f21956deb26245d3ea0
Scotasloth <rossmcc76@gmail.com>
2024-11-17 17:40:05 +0000

    Began writing the web scraping functionality

--- tracker.py
+++ tracker.py
@@ -2,12 +2,21 @@ import sys
 from customtkinter import *
 import pyodbc
 import os
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.keys import Keys
+import time
+import requests
 
 def main():
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")  # Run browser in background 
 
     program_directory = sys.path[0]
     print (program_directory)
-    create(program_directory)
+    #create(program_directory)
+    getWebPage(chrome_options)
 
 def create(dir):
     db_name = 'gamedata.accdb'
@@ -31,6 +40,25 @@ def connect(db, dir):
     database = conn.cursor()
     print("Connection opened")
     return database
+
+def getWebPage(chrome_options):
+    url = ""
+
+    driver = webdriver.Chrome(options=chrome_options)
+
+    try:
+        driver.get(url)
+        time.sleep(3)
+
+        titles = driver.find_elements(By.TAG_NAME, '')
+
+        for title in titles:
+            print("Title Found", title.text)
+    
+    except Exception as e:
+        print("Error", e)
+
+    driver.quit()
 
 if __name__ == '__main__':
     main()
